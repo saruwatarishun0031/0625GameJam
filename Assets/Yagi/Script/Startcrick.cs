ea3df02e68863c2b4150d4a13034d421a46b176b
KRYG <abcdef0509a@gmail.com>
2024-06-24 14:40:29 +0900

    タイトル画面の追加

---
+++ Assets/Yagi/Script/Startcrick.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Startcrick : MonoBehaviour
+{
+    [SerializeField] GameObject _button;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
